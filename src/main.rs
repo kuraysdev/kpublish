fe809f96afe6e2a1cbb891a46b68cc6e14825f26
Егор Абрамов <kurays@kurays.ml>
2023-07-08 01:38:19 +0300

    Web server working

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
+use actix_web::{App, HttpServer, web, Responder};
+
+async fn index() -> impl Responder {
+    "Hello world!"
+}
+
 #[tokio::main]
-async fn main() {
+async fn main() -> std::io::Result<()> {
     println!("Hello, world!");
+    HttpServer::new(|| {
+        App::new().service(
+            // prefixes all resources and routes attached to it...
+            web::scope("/app")
+                // ...so this handles requests for `GET /app/index.html`
+                .route("/index.html", web::get().to(index)),
+        )
+    }).bind(("127.0.0.1", 8080))?
+    .run().await?;
+    Ok(())
 }
