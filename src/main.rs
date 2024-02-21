083977ad1f62d40d708aa4e54a9cdbe48e768c51
Егор Абрамов <me@kurays.dev>
2024-02-21 22:58:01 +0300

    New renderer

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use actix_web::web::Data;
 use actix_web::{App, get, HttpServer, web, Responder, HttpResponse, HttpRequest};
 use std::fs;
 use std::fs::File;
 use std::io::Read;
 use actix_web::middleware::Logger;
 use env_logger::Env;
+use handlebars::Handlebars;
 
 mod render;
 mod fileutil;
@@ -16,7 +18,7 @@ async fn index() -> impl Responder {
 }
 
 #[get("/blog")]
-async fn pages(req: HttpRequest) -> impl Responder {
+async fn pages(req: HttpRequest, hb: web::Data<Handlebars<'_>>) -> impl Responder {
     match fs::read_dir("public") {
         Ok(entries) =>  {
             let links: Vec<String> = entries
@@ -41,7 +43,7 @@ async fn pages(req: HttpRequest) -> impl Responder {
                 .collect();
 
             let combined_links = links.join("\n\n");
-            HttpResponse::Ok().body(render::html("Ссылки", &combined_links))
+            HttpResponse::Ok().body(render::render(hb, "Ссылки", &combined_links))
         },
         Err(_) => HttpResponse::InternalServerError().body("Failed to read directory.")
     }
@@ -49,32 +51,36 @@ async fn pages(req: HttpRequest) -> impl Responder {
 
 
 #[get("/blog/{post}")]
-async fn render_md_file(path: web::Path<String>) -> HttpResponse {
+async fn render_md_file(path: web::Path<String>, hb: web::Data<Handlebars<'_>>) -> HttpResponse {
     let post = path.into_inner();
     match File::open(format!("public/{}.md", post)) {
         Ok(mut file) => {
             let mut contents = String::new();
             file.read_to_string(&mut contents).unwrap();
-            let html_output = render::html(&post, &contents);
+            let html_output = render::render(hb, &post, &contents);
             HttpResponse::Ok().body(html_output)
         },
-        Err(_) => HttpResponse::InternalServerError().body(render::html("НЕт ТаКоГо ФАЙла МЛЯТЬ.", "Создать хочешь? \n\n ПЕРЕХОЧЕШЬ"))
+        Err(_) => HttpResponse::InternalServerError().body(render::render(hb, "НЕт ТаКоГо ФАЙла МЛЯТЬ.", "Создать хочешь? \n\n ПЕРЕХОЧЕШЬ"))
     }
-    
+}
 
-
-
-    
+fn register_templates() -> Data<Handlebars<'static>> {
+    let mut handlebars = Handlebars::new();
+    handlebars.register_templates_directory(".html", "./templates").unwrap();
+    println!("Registered {} templates", handlebars.get_templates().len().to_owned());
+    return web::Data::new(handlebars)
 }
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    println!("Hello, world!");
+    println!("Hello, kpublish!");
+    let handlebars = register_templates();
     env_logger::init_from_env(Env::default().default_filter_or("info"));
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
         .wrap(Logger::default())
         .wrap(Logger::new("%a %{User-Agent}i"))
+        .app_data(handlebars.clone())
         .service(index)
         .service(render_md_file)
         .service(pages)
--- src/render.rs
+++ src/render.rs
@@ -1,69 +1,25 @@
-use horrorshow::Raw;
-use horrorshow::html;
-use horrorshow::helper::doctype;
+use handlebars::Handlebars;
 use pulldown_cmark::html::push_html;
 use pulldown_cmark::{Parser, Options};
+use serde_json::json;
+use actix_web::{web};
 
 //Я люблю сосать член
-pub fn html(name: &str, markdown: &str) -> String {
-    format!(
-        "{}",
-        html! {
-            : doctype::HTML;
-            html {
-                : Raw(&make_header(name));
-                body(class="markdown-body") {
-                    nav {
-                        a(href="/blog") {
-                            
-                        }
-                    }
-                    main {
-                        : Raw(&mark_to_html(format!("# {}\n{}", name, markdown).as_str()));
-                    }
-                    script {
-                        : Raw("hljs.initHighlighting()");
-                    }
-                    footer {
-                        p { : "Author: Egor Abramov" }
-                        p { a(href="mailto:kurays@kurays.ml") { : "me@kurays.dev" } }
-                    }
-                }
-            }
-        }
-    )
+pub fn render(hb: web::Data<Handlebars<'_>>, name: &str, markdown: &str) -> String {
+    let data = json!({
+        "name": name,
+        "content": &mark_to_html(format!("# {}\n{}", name, markdown).as_str())
+    });
+    let body = hb.render("post", &data).unwrap();
+
+    return body;
 }
+
+
 
 fn mark_to_html(markdown: &str) -> String {
     let parser = Parser::new_ext(&markdown, Options::all());
     let mut buffer = String::new();
     push_html(&mut buffer, parser);
     buffer
-}
-
-
-fn make_header(name: &str) -> String {
-    format!("{}",
-        html! {
-            head {
-                link(rel="stylesheet", href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.2.0/github-markdown-dark.min.css");
-                link(rel="stylesheet", href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/github-dark.min.css");
-                script(src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js");
-                meta(charset="utf-8");
-                meta(property="og:title", content=&name);
-                meta(property="og:type", content="website");
-                meta(property="og:url", content="http://blog.kurays.dev");
-                meta(property="og:description", content="Потом сделаю");
-                title { : &name }
-                meta(name="viewport", content="width=device-width, initial-scale=1.0");
-                style {
-                    : "body { width: 80%; margin: 1% auto !important; }";
-                    : "nav { height: 2em }";
-                    : "img { max-width: 80% }";
-                    : "main { background-color: #1c1d2a; border-radius: 10px; padding: 1px 25px; }";
-                    
-                }
-            }
-        }
-    )
 }
