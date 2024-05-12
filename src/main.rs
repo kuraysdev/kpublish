7290789f4cd4817c639134300801a9f68126c09a
Егор Абрамов <me@kurays.dev>
2024-05-13 00:38:31 +0300

    add static files support

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use actix_web::web::Data;
-use actix_web::{get, web, App, HttpResponse, HttpServer};
+use actix_web::{get, web, App, HttpResponse, HttpServer, HttpRequest};
+use actix_files::NamedFile;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
@@ -30,12 +31,12 @@ async fn file_tree() -> HttpResponse {
 
 
 #[get("/{post:.*}")]
-async fn render_md_file(path: web::Path<String>, hb: web::Data<Handlebars<'_>>) -> HttpResponse {
+async fn return_file(req: HttpRequest, path: web::Path<String>, hb: web::Data<Handlebars<'_>>) -> HttpResponse {
     let post = path.into_inner();
     println!("{}", post);
     let requested_path = Path::new("public").join(format!("{}", post));
 
-    let md_file_path = if requested_path.is_dir() || post.is_empty() {
+    let file_path = if requested_path.is_dir() || post.is_empty() {
         requested_path.join("index.md")
     } else if requested_path.extension().is_none() {
         requested_path.with_extension("md")
@@ -43,26 +44,21 @@ async fn render_md_file(path: web::Path<String>, hb: web::Data<Handlebars<'_>>) 
         requested_path
     };
 
-    if md_file_path.exists() && md_file_path.is_file() {
-        // Если файл существует, возвращаем его
+    if !file_path.exists() || !file_path.is_file() {
+        // Если файл не найден, возвращаем ошибку 404
+        return HttpResponse::NotFound().body("404").into();
+    }
+
+    if file_path.extension().unwrap() == "md" {
+        // Если md файл рендерим его
         let mut contents = String::new();
-        let mut file = File::open(md_file_path).unwrap();
+        let mut file = File::open(file_path).unwrap();
         file.read_to_string(&mut contents).unwrap();
         let html_output = render::render(hb, &post, &contents);
         HttpResponse::Ok().body(html_output)
-    } else {
-        // Если файл не найден, возвращаем ошибку 404
-        HttpResponse::InternalServerError().body(render::render(hb, "НЕт ТаКоГо ФАЙла МЛЯТЬ.", "Создать хочешь? \n\n ПЕРЕХОЧЕШЬ"))
+    } else  {
+        NamedFile::open(file_path).unwrap().into_response(&req)
     }
-    // match File::open(format!("public/{}.md", post)) {
-    //     Ok(mut file) => {
-    //         let mut contents = String::new();
-    //         file.read_to_string(&mut contents).unwrap();
-    //         let html_output = render::render(hb, &post, &contents);
-    //         HttpResponse::Ok().body(html_output)
-    //     },
-    //     Err(_) => HttpResponse::InternalServerError().body(render::render(hb, "НЕт ТаКоГо ФАЙла МЛЯТЬ.", "Создать хочешь? \n\n ПЕРЕХОЧЕШЬ"))
-    // }
 }
 
 fn register_templates() -> Data<Handlebars<'static>> {
@@ -84,7 +80,7 @@ async fn main() -> std::io::Result<()> {
         .app_data(handlebars.clone())
         //.service(index)
         .service(file_tree)
-        .service(render_md_file)
+        .service(return_file)
         //.service(pages)
     }).bind(("0.0.0.0", 8080))?
     .run().await?;
